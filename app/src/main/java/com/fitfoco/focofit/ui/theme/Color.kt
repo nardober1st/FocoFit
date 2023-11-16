b597261f9c932c4367439a5241317df22e034574
nardober1st <bernardo.oechsler6@gmail.com>
2023-11-16 00:56:48 -0800

    project created

---
+++ app/src/main/java/com/fitfoco/focofit/ui/theme/Color.kt
@@ -0,0 +1,24 @@
+package com.fitfoco.focofit.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Black = Color(0xFF000000)
+val Black42 = Color(0x6B000000)
+val White = Color(0xFFFFFFFF)
+val SafetyYellow = Color(0xFFECA20C)
+val Orange = Color(0xFFE46A15)
+val YellowGold = Color(0xFFDFC76C)
+val BlueBackground = Color(0xFFEFF8FF)
+val Outline = Color(0xFFC7C7CC)
+val Blue01 = Color(0xFF2B4099)
+val Blue02 = Color(0xFF3BB6FB)
+val Blue03 = Color(0xFF262473)
+val Blue04 = Color(0xFF230753)
