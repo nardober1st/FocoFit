76b400c5a792b286249dcffaccf7327a8bb1ed07
vhboico <hboico@hotmail.com>
2023-11-17 11:49:10 -0300

    commit hidePassword icon

--- app/src/main/java/com/fitfoco/focofit/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/fitfoco/focofit/presentation/login/LoginScreen.kt
@@ -6,13 +6,20 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,9 +27,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -37,6 +47,7 @@ import com.fitfoco.focofit.ui.theme.Black80
 import com.fitfoco.focofit.ui.theme.Blue01
 import com.fitfoco.focofit.ui.theme.Blue03
 import com.fitfoco.focofit.ui.theme.BlueBackground
+import com.fitfoco.focofit.ui.theme.Outline
 import com.fitfoco.focofit.ui.theme.ShapeEdit
 import com.fitfoco.focofit.ui.theme.White
 import com.fitfoco.focofit.viewmodel.LoginViewModel
@@ -56,6 +67,16 @@ fun LoginScreen(
 
     var password by remember {
         mutableStateOf("")
+    }
+
+    var visibility by remember {
+        mutableStateOf(false)
+    }
+
+    val icon = if (visibility) {
+        painterResource(id = R.drawable.baseline_visibility_24)
+    } else {
+        painterResource(id = R.drawable.baseline_visibility_off_24)
     }
 
     Scaffold {
@@ -90,15 +111,38 @@ fun LoginScreen(
                     trailingIcon = { null }
                 )
 
-                EditText(
-                    label = stringResource(id = R.string.password),
+                OutlinedTextField(
                     value = password,
-                    onValueChanged = { password = it },
-                    keyboardType = KeyboardType.Password,
-                    modifier = Modifier.constrainAs(edtPassword) {
-                        top.linkTo(edtEmail.bottom)
+                    onValueChange = { password = it },
+                    label = { Text(text = stringResource(id = R.string.password)) },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 20.dp, end = 20.dp)
+                        .constrainAs(edtPassword) {
+                            top.linkTo(edtEmail.bottom)
+
+                        },
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        containerColor = White,
+                        textColor = Outline,
+                        focusedLabelColor = Blue01,
+                        unfocusedLabelColor = Outline,
+                        focusedBorderColor = Blue01,
+                        unfocusedBorderColor = Outline,
+                        cursorColor = Blue01
+                    ),
+                    maxLines = 1,
+                    shape = ShapeEdit.small,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password
+                    ),
+                    trailingIcon = {
+                        IconButton(onClick = { visibility = !visibility }) {
+                            Icon(painter = icon, contentDescription = "")
+                        }
                     },
-                    trailingIcon = { null }
+                    visualTransformation = if (visibility) VisualTransformation.None
+                    else PasswordVisualTransformation()
                 )
 
                 ButtonEdit(onClick = {
